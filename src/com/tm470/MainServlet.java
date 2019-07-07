53eb5f97216e6e7cb494c8e1761cf4cdb0d0c1f4
Ewa Panonko <ewa.panonko@gmail.com>
2019-07-07 21:57:30 +0100

    Added table 'Setup' to MainServlet

--- src/com/tm470/MainServlet.java
+++ src/com/tm470/MainServlet.java
@@ -13,6 +13,7 @@ import java.sql.Date;
 
 public class MainServlet extends HttpServlet {
 
+
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
 
@@ -23,12 +24,25 @@ public class MainServlet extends HttpServlet {
         PrintWriter out = res.getWriter();
         out.println("<html><head>");
         out.println("<title>Tab title - Test tab title</title>");
-        out.println("\t<style>body { font-family: 'Times New Roman', " +
-                " 'Times' " +
-                "serif ; " +
-                "font-size: 14px; }</style>");
+      //  out.println("\t<style>body { font-family: 'Times New Roman', " +
+       //         " 'Times' " +
+       //         "serif ; " +
+       //         "font-size: 14px; }</style>");
+        out.println("<style>body { font-family: Arial, sans-serif; font-size: 14px}</style>");
+        out.println(" <style type=\"text/css\">");
+        out.println("<style>.table_setup  {font-weight:bold; border-collapse:collapse;border-spacing:0;}</style>");
+        out.println("<style>.table_setup td{padding:6px 20px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}</style>");
+
         out.println("</head>");
-        out.println("<h1>Test header - servlet</h1>");
+
+
+
+
+
+
+
+
+     /*   out.println("<h1>Test header - servlet</h1>");
         out.println("<body>");
 
         String doctype = "<!DOCTYPE html>\n";
@@ -54,13 +68,72 @@ public class MainServlet extends HttpServlet {
         Double calc = (13.00 - ProductRate)/45;
         out.println("<p>Some calculation: ProductRate + (13-ProductRate)+45 =  " + calc + "</p>");
         out.println("</fieldset>");
-
+*/
         out.println("<br><br>");
         out.println("<button onclick=goBack()>Go Back</button>");
         out.println("<script>");
         out.println("function goBack() { window.history.back() }");
         out.println("</script>");
-        out.println("</body></html>");
+
+        out.println("<br><br>");
+        out.println("============================================================");
+
+
+
+      //  out.println("</body></html>");
+
+
+
+
+
+        out.println("<h1>Savings Interest Calculator</h1>");
+        out.println("<h2><b>Setup</b></h2>");
+        out.println("<table class=\"table_setup\">");
+        out.println("<tr>");
+        out.println(" <td>Product Launch Date (format 2012-12-12):</td>");
+        out.println("<td><input type=\"date\" id=\"ProductLaunchDate\" name=\"ProductLaunchDate\" required/></td>");
+        out.println(" <td> Product Rate :</td>");
+        out.println("<td><input type=\"number\" id=\"ProductRate\" name=\"ProductRate\" step=\".01\" required/></td>");
+        out.println("<td rowspan=\"2\">Deposit Value :</td>");
+        out.println("<td rowspan=\"2\"><input type=\"number\" id=\"DepositValue\" name=\"DepositValue\" step=\".01\" required/></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println(" <td>Product End Date (format 2012-12-12):</td>");
+        out.println("<td><input type=\"date\" id=\"ProductEndDate\" name=\"ProductEndDate\" required/></td>");
+        out.println("<td>Monthly Rate :</td>");
+        out.println("<td><input type=\"number\" id=\"MonthlyRate\" name=\"MonthlyRate\" step=\".01\" required/></td>");
+        out.println("</tr>");
+        out.println(" </table>");
+        out.println(" <br/><br/>");
+      //  out.println("");
+      //  out.println("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         out.close();
     }
 }
