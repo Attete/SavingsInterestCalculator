b85b8db3b6b08f51654b017595b7f89d9db75a9e
Ewa Panonko <ewa.panonko@gmail.com>
2019-07-09 04:55:55 +0100

    Added test dates to the 'Capitalisation Dates' column of the 'Calculations' table in index.html

--- src/com/tm470/MainServlet.java
+++ src/com/tm470/MainServlet.java
@@ -22,7 +22,7 @@ public class MainServlet extends HttpServlet {
 
         out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
         out.println("<!DOCTYPE html>");
-        out.println("<head>");
+   out.println("<html><head>");
         out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
         out.println("<title>SIC Result</title>");
         out.println("<style>body { font-family: Arial, sans-serif; font-size: 14px}</style>");
@@ -53,11 +53,18 @@ public class MainServlet extends HttpServlet {
 
         LocalDate productLaunchDate, productEndDate;//Setup dates
         double productRate= 0.0, depositValue = 0.0, monthlyRate;//setup values
+        LocalDate date1, date2, date3, date4;// to store Capitalisation Dates from 'Calculations' table in index.html
 
 
         productLaunchDate = LocalDate.parse(req.getParameter("ProductLaunchDate"));//storing the productLaunchDate from index.html
         productEndDate = LocalDate.parse(req.getParameter("ProductEndDate"));//storing the productEndDate from index.html
         productRate = Double.parseDouble(req.getParameter("ProductRate"));//parsing String to double
+        date1 = LocalDate.parse(req.getParameter("date1"));//Capitalisation date1 from 'Calculations' table in index.html
+        date2 = LocalDate.parse(req.getParameter("date2"));//Capitalisation date2 from 'Calculations' table in index.html
+        date3 = LocalDate.parse(req.getParameter("date3"));//Capitalisation date3 from 'Calculations' table in index.html
+        date4 = LocalDate.parse(req.getParameter("date4"));//Capitalisation date4 from 'Calculations' table in index.html
+
+//LocalDate somedate = LocalDate.parse("1999-09-09");
 
 
         //Reversing ProductLaunchDate to display in dd/mm/yyyy format in Setup table
@@ -71,6 +78,31 @@ public class MainServlet extends HttpServlet {
         String productEndDate2 = req.getParameter("ProductEndDate").substring(5,7);
         String productEndDate3 = req.getParameter("ProductEndDate").substring(8);
         String productEndDateReversed = productEndDate3 + "/" + productEndDate2 + "/" + productEndDate1;
+
+        //Reversing date1 to display in dd/mm/yyyy format in Calculations table
+        String date1a = req.getParameter("date1").substring(0,4);
+        String date1b = req.getParameter("date1").substring(5,7);
+        String date1c = req.getParameter("date1").substring(8);
+        String date1Reversed = date1c + "/" + date1b + "/" + date1a;
+
+        //Reversing date2 to display in dd/mm/yyyy format in Calculations table
+        String date2a = req.getParameter("date2").substring(0,4);
+        String date2b = req.getParameter("date2").substring(5,7);
+        String date2c = req.getParameter("date2").substring(8);
+        String date2Reversed = date2c + "/" + date2b + "/" + date2a;
+
+        //Reversing date3 to display in dd/mm/yyyy format in Calculations table
+        String date3a = req.getParameter("date3").substring(0,4);
+        String date3b = req.getParameter("date3").substring(5,7);
+        String date3c = req.getParameter("date3").substring(8);
+        String date3Reversed = date3c + "/" + date3b + "/" + date3a;
+
+        //Reversing date4 to display in dd/mm/yyyy format in Calculations table
+        String date4a = req.getParameter("date4").substring(0,4);
+        String date4b = req.getParameter("date4").substring(5,7);
+        String date4c = req.getParameter("date4").substring(8);
+        String date4Reversed = date4c + "/" + date4b + "/" + date4a;
+
 
 
 ////////////////// SETUP TABLE //////////////////////////////////////////////
@@ -87,14 +119,14 @@ public class MainServlet extends HttpServlet {
         out.println("<h2><b>Setup</b></h2>");
         out.println("<table class=\"cal\">");
         out.println("<tr>");
-        out.println("<th class=\"cal-xx12\">Product Launch Date (format 2012-12-12):</th>");
+        out.println("<th class=\"cal-xx12\">Product Launch Date :</th>");
         out.println("<td class=\"cal-xx12\">" + productLaunchDateReversed + "</th> ");
         out.println("<th class=\"cal-xx12\">Product Rate :</th>");
         out.println("<td class=\"cal-xx12\">" + req.getParameter("ProductRate") + "</th>");
         out.println("<th rowspan=\"2\" class=\"cal-xx12\"> Deposit Value : " + " " + req.getParameter("DepositValue") + "</th>");
         out.println("</tr>");
         out.println("<tr>");
-        out.println("<th class=\"cal-xx12\">Product End Date (format 2012-12-12):</td>");
+        out.println("<th class=\"cal-xx12\">Product End Date :</td>");
         out.println("<td class=\"cal-zz12\">" + productEndDateReversed + "</td>");
         out.println("<th class=\"cal-zz12\">Monthly Rate :</td>");
         out.println("<td class=\"cal-zz12\">" + monthlyRate + "</td>");
@@ -105,7 +137,6 @@ public class MainServlet extends HttpServlet {
 
 ////////////////// CALCULATIONS TABLE //////////////////////////////////////////////
         out.println("<h2><b>Calculations</b></h2>");
-
         out.println("<table class=\"cal\">");
         out.println("<tr>");
         out.println("<th class=\"cal-xx12\">Capitalisation Dates</th>");
@@ -115,28 +146,28 @@ public class MainServlet extends HttpServlet {
         out.println("<th class=\"cal-xx12\">Daily Accrual</th>");
         out.println("</tr>");
         out.println("<tr>");
-        out.println("<td class=\"cal-zz12\">date 1</td>");
+        out.println("<td class=\"cal-zz12\">" + date1Reversed + "</td>");
         out.println("<td class=\"cal-zz12\">number of days 1</td>");
         out.println("<td class=\"cal-zz12\">Interest 1</td>");
         out.println("<td class=\"cal-zz12\">Compounded 1</td>");
         out.println("<td class=\"cal-zz12\">Accrual 1</td>");
         out.println("</tr>");
         out.println("<tr>");
-        out.println("<td class=\"cal-yy12\">date 2</td>");
+        out.println("<td class=\"cal-yy12\">" + date2Reversed + "</td>");
         out.println("<td class=\"cal-yy12\">number of days 2</td>");
         out.println("<td class=\"cal-yy12\">Interest 2</td>");
         out.println("<td class=\"cal-yy12\">Compounded 2</td>");
         out.println("<td class=\"cal-yy12\">Accrual 2</td>");
         out.println("</tr>");
         out.println("<tr>");
-        out.println("<td class=\"cal-yy12\">date 3</td>");
+        out.println("<td class=\"cal-yy12\">" + date3Reversed + "</td>");
         out.println("<td class=\"cal-yy12\">number of days 3</td>");
         out.println("<td class=\"cal-yy12\">Interest 3</td>");
         out.println("<td class=\"cal-yy12\">Compounded 3</td>");
         out.println("<td class=\"cal-yy12\">Accrual 3</td>");
         out.println("</tr>");
         out.println("<tr>");
-        out.println("<td class=\"cal-yy12\">date 4</td>");
+        out.println("<td class=\"cal-yy12\">" + date4Reversed + "</td>");
         out.println("<td class=\"cal-yy12\">number of days 4</td>");
         out.println("<td class=\"cal-yy12\">Interest 4</td>");
         out.println("<td class=\"cal-yy12\">Compounded 4</td>");
